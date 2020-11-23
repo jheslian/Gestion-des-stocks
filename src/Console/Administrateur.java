6f60bdb54d7857062202fd9c601f78cc3ca3603d
jesusa <justinjhes@gmail.com>
2020-11-23 14:45:47 +0100

    story 0

---
+++ src/Console/Administrateur.java
@@ -0,0 +1,11 @@
+package Console;
+
+public class Administrateur {
+    final String nom = "jesusa";
+    final String motDePasse = "123456";
+
+    Administrateur(){
+
+
+    }
+}
--- src/com/jesusasadey/Main.java
+++ src/com/jesusasadey/Main.java
@@ -1,8 +1,24 @@
 package com.jesusasadey;
 
+import Console.Gestion;
+
+import java.awt.*;
+
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        EventQueue.invokeLater( new Runnable() {
+            public void run() {
+                try {
+                    new Gestion();
+                    //userInterface.setVisible( true );
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("L'application à detecter une erreur de type " + e.getMessage());
+                }
+            }
+        });
+
     }
 }
